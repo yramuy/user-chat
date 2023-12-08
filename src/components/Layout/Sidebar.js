3829f1991b6defc5a95c16de60a8ad17b0727294
ramuy <ramuy@prospectatech.com>
2023-12-08 18:40:21 +0530

    2nd commit
    Signed-off-by: ramuy <ramuy@prospectatech.com>

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,54 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { GetApiService, PostApiService } from "../Apis/ApiService";
 
 const Sidebar = () => {
 
+    const [menuList, setMenuList] = useState([]);
+    const [subMenuList, setSubMenuList] = useState([]);
+
     useEffect(() => {
-        fetchMenus('/rest_apis/v1/sidebarMenus');
+        fetchMenus('/ProjectApis/v1/chatMenus');
     }, []);
 
     const fetchMenus = async (url) => {
-
         try {
-            await fetch(url, {
-                method: 'GET',
-                headers: {
-                    "Authorization": "b8416f2680eb194d61b33f9909f94b9d",
-                    "Content-Type": "application/json",
-                },
-            }).then((response) => response.json()).then((data) => {
+
+            await GetApiService(url).then((data) => {
+                console.log("list", data);
+                console.log("menus", data.menus);
+                console.log("sub menus", data.menus[0].sub_menus);
 
                 if (data.status === 1) {
-
-                    console.log(data);
-
-                    // const userData = data.userDetails;
-                    // sessionStorage.setItem("userId", userData.user_id);
-                    // sessionStorage.setItem("userName", userData.user_name);
-                    // sessionStorage.setItem("userRoleId", userData.user_role_id);
-                    // sessionStorage.setItem("userRole", userData.role_name);
-                    // sessionStorage.setItem("userImg", userData.image);
-                    // sessionStorage.setItem("empName", userData.emp_name);
-                    // sessionStorage.setItem("empNumber", userData.emp_number);
-
-                    // setShowMsg(false);
-                    // navigate('/home', { replace: true });
-                } else {
-                    // setShowMsg(true);
-                    // navigate('/', { replace: true });
+                    setMenuList(data.menus);
                 }
-
-                console.log(data.message);
-
             });
-        } catch (error) {
-            
-        }
 
-        // await ApiService(url).then((data) => {
-        //     console.log(data)
-        // });
-    }
+        } catch (error) {
+
+        }
+    };
 
     return (
         <>
@@ -58,7 +37,28 @@ const Sidebar = () => {
 
             <ul className="list-unstyled components">
                 <p>Dummy Heading</p>
-                <li className="active">
+                {
+                    menuList.map((menu) => (
+                        <li className="active" key={menu.id}>
+                            <a href={`#${menu.name}`} data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">{menu.name}</a>
+                            <ul className="collapse list-unstyled" id={menu.name}>
+                                {
+                                    menu.sub_menus.map((submenu) => (
+
+                                        <li key={submenu.sub_menu_id}>
+                                            <Link to={`/user-chat/${submenu.sub_menu_id}`}>{submenu.sub_menu}</Link>
+                                        </li>
+
+
+                                    ))
+                                }
+
+                            </ul>
+
+                        </li>
+                    ))
+                }
+                {/* <li className="active">
                     <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Groups</a>
                     <ul className="collapse list-unstyled" id="homeSubmenu">
                         <li>
@@ -86,7 +86,7 @@ const Sidebar = () => {
                             <a href="#">Page 3</a>
                         </li>
                     </ul>
-                </li>
+                </li> */}
 
             </ul>
         </>
