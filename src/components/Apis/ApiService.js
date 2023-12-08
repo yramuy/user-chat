40ff75d640a46000cbdf5508a85fabcc65fe8157
ramuy <ramuy@prospectatech.com>
2023-12-08 18:41:15 +0530

    modified in api's
    Signed-off-by: ramuy <ramuy@prospectatech.com>

---
+++ src/components/Apis/ApiService.js
@@ -0,0 +1,29 @@
+
+export const GetApiService = async (url) => {
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": "b8416f2680eb194d61b33f9909f94b9d",
+            "Content-Type": "application/json",
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+};
+
+export const PostApiService = async (url, body) => {
+    const response = await fetch(url, {
+        method: "POST",
+        body: body,
+        headers: {
+            "Authorization": "b8416f2680eb194d61b33f9909f94b9d",
+            "Content-Type": "application/json",
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+}
