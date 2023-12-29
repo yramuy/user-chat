031bd0261fa059a62311541aadfb089815886f47
yramuy <ramuy77290@gmail.com>
2023-12-29 10:52:43 +0530

    modified
    Signed-off-by: yramuy <ramuy77290@gmail.com>

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
     const navigate = useNavigate();
 
     const [open, setOpen] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     const handleLogout = () => {
         setOpen(true);
@@ -30,6 +31,10 @@ const Header = () => {
         navigate('/', { replace: true });
     }
 
+    const handleAddMenu = () => {
+        setMenu(true);
+    }
+
     return (
         <>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -42,6 +47,7 @@ const Header = () => {
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style={{ left: '-6rem' }}>
                                 <a className="dropdown-item" href="#"><i class="fa-solid fa-user"></i> Profile</a>
+                                <a className="dropdown-item" href="#" onClick={handleAddMenu}><i class="fa-solid fa-square-plus"></i> Add Menu</a>
                                 <a className="dropdown-item" href="#" onClick={handleLogout}><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
                             </div>
                         </div>
@@ -66,6 +72,53 @@ const Header = () => {
                 <DialogActions>
                     <button type="button" class="btn btn-primary" onClick={handleYes} autoFocus>Yes</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onClick={handleClose}>No</button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog
+                open={menu}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title">
+                    {"Add Menu"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <div class="row">
+                            <div class="col-4">
+                                <label for="exampleDataList" class="form-label">Menu</label>
+                            </div>
+                            <div class="col-5">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                    <label class="form-check-label" for="inlineRadio1">Groups</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                    <label class="form-check-label" for="inlineRadio2">Individual</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4"><label for="exampleDataList" class="form-label">Sub Menu</label></div>
+                            <div class="col-5">
+
+                                <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..." />
+                                <datalist id="datalistOptions">
+                                    <option value="San Francisco" />
+                                    <option value="New York" />
+                                    <option value="Seattle" />
+                                    <option value="Los Angeles" />
+                                    <option value="Chicago" />
+                                </datalist>
+                            </div>
+                        </div>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <button type="button" class="btn btn-success" onClick={handleYes} autoFocus>Save</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onClick={handleClose}>Cancel</button>
                 </DialogActions>
             </Dialog>
 
---
+++ src/components/Login.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import UserImg from '../images/user1.jpg';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { PostApiService } from "./Apis/ApiService";
+
+const Login = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const [showMsg, setShowMsg] = useState(false);
+    const [chatId, setChatId] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            const url = '/ProjectApis/v1/loginWithMobNumOrUsrname';
+            const body = JSON.stringify({
+                mobnumber: "",
+                username: userName,
+                password: password,
+            });
+
+            await PostApiService(url, body).then((data) => {
+                if (data.status === 1) {
+
+                    const userData = data.userDetails;
+                    sessionStorage.setItem("userId", userData.user_id);
+                    sessionStorage.setItem("userName", userData.user_name);
+                    sessionStorage.setItem("userRoleId", userData.user_role_id);
+                    sessionStorage.setItem("userRole", userData.role_name);
+                    sessionStorage.setItem("userImg", userData.image);
+                    sessionStorage.setItem("empName", userData.emp_name);
+                    sessionStorage.setItem("empNumber", userData.emp_number);
+
+                    getChatIdByUserId(userData.user_id);
+                } else {
+                    setShowMsg(true);
+                    navigate('/', { replace: true });
+                }
+            });
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const getChatIdByUserId = async (id) => {
+
+        const url = '/ProjectApis/v1/chatDataByUserId';
+        const body = JSON.stringify({
+            user_id: id
+        });
+
+        await PostApiService(url, body).then((data) => {
+            console.log(data)
+            var res = data.user;
+            console.log("response", res)
+            if (data.status === 1) {
+                var chat_id = res[0]['id'];
+                setShowMsg(false);
+                navigate(`/user-chat/${chat_id}`, { replace: true });
+            }
+        });
+    }
+
+
+    return (
+        <>
+
+            <div className="container">
+                <div className="row m-5 no-gutters shadow-lg">
+                    <div className="col-md-6 d-none d-md-block">
+                        <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" className="img-fluid" style={{ height: '509px', width: '495px' }} />
+                    </div>
+                    <div className="col-md-6 bg-white p-5">
+
+                        <div className="form-style">
+                            <img src={UserImg} alt="Avator" style={{ height: '117px' }} />
+                            <form>
+                                <div className="form-group pb-3">
+                                    <input type="email"
+                                        placeholder="Email or Username"
+                                        className="form-control"
+                                        id="username"
+                                        name="username"
+                                        value={userName}
+                                        onChange={(e) => setUserName(e.target.value)} />
+                                </div>
+                                <div className="form-group pb-3">
+                                    <input type="password"
+                                        placeholder="Password"
+                                        className="form-control"
+                                        id="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)} />
+                                </div>
+
+                                <div className="pb-2">
+                                    <button type="submit" className="btn btn-dark w-100 font-weight-bold mt-2" onClick={handleLogin}>Login</button>
+                                </div>
+                            </form>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Login;
